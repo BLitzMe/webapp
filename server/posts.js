e3e8525ca0f949af4b54b40d000b2ee7f9896f86
sru <sru>
2018-07-05 15:40:50 +0200

    Server functionality: Database connection, 'start'-parameter, 'location'-parameter

--- server/posts.js
+++ server/posts.js
@@ -1,6 +1,7 @@
 //imports
 var fs = require('fs');
 var url = require('url');
+var Datastore = require('nedb');
 
 //defining the scope for calls from external files
 module.exports = { 
@@ -9,24 +10,20 @@ module.exports = {
 		}
 }
 
-//handling the initial call
+//handling the call
 function handleCall(request, response) {
-	example = {
-        "location" : "Clausthal-Zellerfeld",
-        "id" : "sdhf78wei",
-        "title" : "Leckere Tomaten",
-        "picture" : "/img/tomatoes.png"
-    }
-    
-    returnData = new Array();
-    
-    for (i = 0; i < 20; i++) {
-        returnData.push(JSON.parse(JSON.stringify(example))); //deep copy
-        returnData[i].title += (" " + i);
-        returnData[i].id += (" " + i);        
-    }
-    
-    response.writeHead(200, {'Content-Type': 'text/html'});
-	response.write(JSON.stringify(returnData));
-	response.end();	
+		
+	var startingPost = 0;
+	if (request.query.start)	
+		startingPost = parseInt(request.query.start);
+	
+	postDB = new Datastore({ filename: "posts.db", autoload: true });
+	postDB.find({"location" : request.query.location}).sort({date: -1}).exec(function(err, docs) {
+		
+		docs = docs.slice(startingPost, startingPost + 20);
+		
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write(JSON.stringify(docs));
+		response.end();	
+	});
 }
--- server/server.js
+++ server/server.js
@@ -2,16 +2,78 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var postsAPI = require('./posts.js');
+var Datastore = require('nedb');
+var express = require('express');
 
-http.createServer(server).listen(3001);
 
-
-function server(request, response) {
-	var q = url.parse(request.url, true);
-	if (q.pathname.startsWith("/posts"))
-		postsAPI.handlePostsAPICall(request, response);
-	else {
-		response.writeHead(404, {'Content-Type': 'text/html'});
-		response.end();
-	}
+if (false === fs.existsSync("posts.db")) {
+    setupDatabase();
 }
+
+var server = express();
+
+server.get("/posts", postsAPI.handlePostsAPICall);
+
+server.listen(3001);
+
+
+
+
+function setupDatabase(postDBpath) {
+	
+	console.log("creating new database...");
+		
+	var sleep = require('sleep');
+	
+    postDB = new Datastore({ filename: "posts.db", autoload: true });
+	example = {
+        "location" : "Clausthal-Zellerfeld",
+        "id" : "sdhf78wei",
+        "title" : "Leckere Tomaten",
+        "picture" : "/img/tomatoes.png"
+    }
+	
+	for(i = 99; i >=0; i--) {
+		item = JSON.parse(JSON.stringify(example));		
+		item.title = "Leckere Tomaten " + i;
+		
+		item.date = new Date();
+		postDB.insert(item, function (err, newDoc) {});
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+		console.log("item " + i +" created");
+	}  
+	
+	console.log("new database created.");
+}
+
+
