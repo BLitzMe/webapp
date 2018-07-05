41fb52169d98d6902a937f544ee29ad9c72216d0
sru <sru>
2018-07-05 20:12:38 +0200

    included /comments endpoint in server, new mock data

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-var express = require('express');var http = require('http');
+var express = require('express');
 var postsAPI = require('./posts.js');
+var commentsAPI = require('./comments.js');
 
 
 var server = express();
 
 server.get("/posts", postsAPI.handlePostsAPICall);
+server.get("/comments", commentsAPI.handleCommentsAPICall);
 
 server.listen(3001);
 console.log("server active. waiting for connections...")
-
